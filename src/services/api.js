65da6f40ac03510dec8a070b73395d0f9e7ec9e0
Marcos Fornari <marcos-fornari@ubots.com.br>
2021-10-06 09:10:52 -0300

    update creature

--- src/services/api.js
+++ src/services/api.js
@@ -17,12 +17,6 @@ api.getCreatures = async () => {
 
 api.getCreature = async id => {
     return new Promise(async (resolve, reject) => {
-        return resolve({
-            id: '1',
-            name: 'Demon',
-            exp: 6000,
-            life: 6000,
-        })
         api.get('/creature/' + id)
             .then(response => {
                 if (response.status === 200) resolve(response.data)
